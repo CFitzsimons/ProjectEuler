a5fb866f28df4e9e0db8e945e3b648c7fd9d439b
Colin Fitzsimons <colin.fitzsimons3@mail.dcu.ie>
2013-02-12 21:35:47 +0000

    Updated question two
    Question two now produces the correct answer.

--- Question2/Fibonacci.cpp
+++ Question2/Fibonacci.cpp
@@ -6,21 +6,27 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int, int);
+
 int main(){
-    int fibonacci(int, int);
     cout << fibonacci(1,1);
     
     return 0;
 }
-
+/*
+* Method recurivly calls itself to progress in the fibonacci sequence.
+* Once the 'second' value exceeds four-million the program begins to
+* return all even value terms until reaching the end.
+*/
 int fibonacci(int first, int second){
-    if(second >= 1000000)
-        return 0;
+    if(second >= 4000000)
+        return first;
     else{
         int temp = fibonacci(second, first+second);
+        //Check to see if the number is even
         if((first) % 2 == 0)
             return temp + first;
-        else
+        else //Don't add anything if it isn't
             return temp;
     }
 }
