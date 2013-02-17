d5b4e7d5eb59762e0cebe40f091a318f21373a39
Colin Fitzsimons <colin.fitzsimons3@mail.dcu.ie>
2013-02-17 21:48:35 +0000

    Added some comments to question 8

--- Question8/Product.cpp
+++ Question8/Product.cpp
@@ -19,9 +19,13 @@ int main(){
     readFile >> number;
     int largest = -1;
     int temp = 1;
+    //Loops for the length of the string, negative the amount
+    //of digits we take into account for each cycle (5).
     for(string::size_type i = 0; i < number.length()-5; i++){
+        //This loop grabs the product of the next 5 digits.
         for(short k = i; k < i+5;k++)
             temp *= parseShort(number[k]);
+        //Check the product against the previous largest
         if(temp > largest)
             largest = temp;
         temp = 1;
@@ -36,5 +40,6 @@ int main(){
 *                character to a short value.  Assumes input is correct.
 */
 short parseShort(char convert){
+    //Decimal ASCII numbers start at 48.
     return convert - 48;
 }
